5a96150331e0ec02f7864a0422a27ee823890879
meaurovind <34267240+meaurovind@users.noreply.github.com>
2020-08-25 16:46:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,31 +2,63 @@ provider "google" {
   credentials = file("account.json")
   project     = var.project
   region      = var.region
- 
 }
 
-resource "google_logging_folder_sink" "my-sink" {
-  name   = "my-sink"
-  folder = google_folder.my-folder.name
-
-  # Can export to pubsub, cloud storage, or bigquery
-  destination = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
-
-  # Log all WARN or higher severity messages relating to instances
-  filter = "resource.type = gce_instance AND severity >= WARN"
+data "template_file" "default" {
+  template = file("scripts/install.sh")
 }
 
-resource "google_storage_bucket" "log-bucket" {
-  name = "folder-loggingnewn-buckets"
+resource "google_compute_firewall" "default" {
+  name    = "nginx-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["nginx"]
 }
 
-resource "google_project_iam_binding" "log-writer" {
-  role = "roles/storage.objectCreator"
+resource "google_compute_instance" "default" {
+  name         = "nginx"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
 
-  members = [
-    google_logging_folder_sink.my-sink.writer_identity,
-  ]
+  tags = ["nginx"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+
+  metadata_startup_script = data.template_file.default.rendered
+
+  service_account {
+    scopes = ["logging-write"]
+  }
 }
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id  = "nginx_logs"
+  description = "NGINX Access Logs"
+  location    = "US"
+}
+
 resource "google_logging_project_sink" "default" {
   name                   = "nginx-logs"
   destination            = "bigquery.googleapis.com/projects/${var.project}/datasets/${google_bigquery_dataset.default.dataset_id}"
@@ -41,5 +73,3 @@ resource "google_project_iam_binding" "default" {
     google_logging_project_sink.default.writer_identity,
   ]
 }
-
-
