72493826ae3c7081a1225bec64d5afcc430c56d5
meaurovind <34267240+meaurovind@users.noreply.github.com>
2020-08-26 16:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,74 +5,9 @@ provider "google" {
   region      = var.region
 }
 
-data "template_file" "default" {
-  template = file("scripts/install.sh")
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "nginix-firewall"
-  network = google_compute_network.default.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "8080", "1000-2000"]
-  }
-
-  source_tags = ["web"]
-}
-
-resource "google_compute_network" "default" {
-  name = "niginx-network"
-}
-
-
-resource "google_compute_instance" "default" {
-  name         = "nginx"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-
-  tags = ["nginx"]
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-1804-lts"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-    }
-  }
-
-  metadata_startup_script = data.template_file.default.rendered
-
-  service_account {
-    scopes = ["logging-write"]
-  }
-}
-
-resource "google_bigquery_dataset" "default" {
-  dataset_id  = "nginx_log"
-  description = "Nginx Access Logs"
-  location    = "US"
-}
-
-resource "google_logging_project_sink" "default" {
-  name                   = "nginx-logs"
-  destination            = "bigquery.googleapis.com/projects/${var.project}/datasets/${google_bigquery_dataset.default.dataset_id}"
-  filter                 = "resource.type = gce_instance AND logName = projects/${var.project}/logs/nginx-access"
-  unique_writer_identity = true
+project = "${google_project.project.project_id}"
+filter = "resource.type = project" AND logName = projects/${var.project}/logs/nginx-access"
+  
 }
 
 resource "google_project_iam_binding" "default" {
