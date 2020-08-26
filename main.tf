21b1d25ebc3ef214e36ac3e14948296a2e2f85d8
meaurovind <34267240+meaurovind@users.noreply.github.com>
2020-08-26 17:29:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  access_token = "ya29.a0AfH6SMA1nnWGQ3Q8ZtjVNRUwv7SW7AgrFVU1lY7LKKVqIbTQPRlDjysSmfrf5pkuinZvHXapopFrNLY8or-2X6vHu_O5zLUdW-6hpV786WzsEbZ75OLIOhwUniVxcKXGfg0B5QP85W_oUFg_u9T_89Np96Cis0kGbM0q6pdtAum6zVfGTFNkomaZH69Qo4GihoM_go96IihzBm5Cu3sarzz8470jFuef0CF9x8snQ1p2hYkSRWFgv15Kit8UXXs3om6FTJ93Kd1DDw"
   credentials = file("account.json") 
   project     = var.project
   region      = var.region
