8539746cf79160428c88b1786ae5e4c35e445962
meaurovind <34267240+meaurovind@users.noreply.github.com>
2020-08-31 21:20:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,43 @@
-provider "google" {
-  credentials = file("account.json") 
+ provider "google" {
+  credentials = file("account.json")
   project     = var.project
   region      = var.region
+
 }
 
-resource "google_bigquery_dataset" "default" {
-  dataset_id  = "CIML_logs"
-  description = "NGINX Access Logs"
-  location    = "US"
+
+resource "google_bigquery_dataset" "ProjectLogging" {
+    dataset_id = "ProjectLogging"
+    friendly_name = "ProjectLogging"
+    location = "US"
+    project = var.project
+
+
+    access = [
+      {  role = "WRITER"
+        user_by_email = "${google_logging_project_sink.ProjectSink.writer_identity}"
+   ] }
 }
 
-resource "google_logging_project_sink" "default" {
-  name                   = "CISinkl-logs"
-  destination            = "bigquery.googleapis.com/projects/${var.project}/datasets/${google_bigquery_dataset.default.dataset_id}"
-  filter                 = "resource.type = gce_instance AND logName = projects/${var.project}/logs/brewsim-access"
-  
-  
+resource "google_logging_project_sink" "ProjectSink" {
+    name = "ProjectLogging"
+    destination = "bigquery.googleapis.com/projects/var.project/datasets/instance-activity"
+    project = var.project
+    filter = "resource.type = project"
+    depends_on = ["google_bigquery_dataset.ProjectLogging"]
+    unique_writer_identity = true
 }
 
-resource "google_project_iam_binding" "default" {
-  role = "roles/bigquery.dataEditor"
 
-  members = [
-    google_logging_project_sink.default.writer_identity,
-  ]
+resource "google_logging_project_sink" "my-sink" {
+  name = "my-pubsub-instance-sink"
+
+  # Can export to pubsub, cloud storage, or bigquery
+  destination = "pubsub.googleapis.com/projects/my-project/topics/instance-activity"
+
+  # Log all WARN or higher severity messages relating to instances
+  filter = "resource.type = gce_instance AND severity >= WARN"
+
+  # Use a unique writer (creates a unique service account used for writing)
+  unique_writer_identity = true
 }
