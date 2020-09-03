b6cdb1f3ac5bed032743b970e4a0c867391f133c
meaurovind <34267240+meaurovind@users.noreply.github.com>
2020-09-03 20:23:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 variable "project" {
   default = "ci-pipeline-276501"
 }
+variable "organization_id" {
+ 
+  type        = number
+  default     = 142352932669
+}
 
 variable "region" {
   default = "us-central1"
